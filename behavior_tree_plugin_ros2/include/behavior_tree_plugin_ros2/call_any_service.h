6dbd59d73d2c0da83aa7072942c837cdd908b913
Mahdi <57127950+mdirzpr@users.noreply.github.com>
2024-07-01 22:38:37 +0200

    Update call_any_service.h

--- behavior_tree_plugin_ros2/include/behavior_tree_plugin_ros2/call_any_service.h
+++ behavior_tree_plugin_ros2/include/behavior_tree_plugin_ros2/call_any_service.h
@@ -2,10 +2,10 @@
 
 #include "behaviortree_cpp_v3/action_node.h"
 #include <rclcpp/rclcpp.hpp>
-#include <ros_babel_fish/babel_fish.hpp>
-#include <ros_babel_fish/message_types.hpp>
+#include <ros2_babel_fish/babel_fish.hpp>
+#include <ros2_babel_fish/message_types.hpp>
 
-namespace behavior_tree_plugin_ros
+namespace behavior_tree_plugin_ros2
 {
 class CallAnyService : public BT::SyncActionNode
 {
@@ -14,18 +14,18 @@ public:
   static BT::PortsList getPorts(std::string service_type);
   BT::NodeStatus tick() override;
   static void makePortList(BT::PortsList& local_port_list, const BT::PortDirection& port_direction,
-                           const ros_babel_fish::MessageTemplate::ConstPtr message_template,
+                           const ros2_babel_fish::MessageTemplate::ConstPtr message_template,
                            const std::string& prefix = "");
-  void makeRequest(ros_babel_fish::Message& request, const std::string& prefix);
-  void extractResponse(ros_babel_fish::Message& response, const std::string& prefix);
+  void makeRequest(ros2_babel_fish::Message& request, const std::string& prefix);
+  void extractResponse(ros2_babel_fish::Message& response, const std::string& prefix);
 
 private:
   const std::string service_type_;
   rclcpp::Node::SharedPtr node_;  // ROS2 node
 
   template <typename T>
-  void printArray(const ros_babel_fish::ArrayMessage<T>& message);
+  void printArray(const ros2_babel_fish::ArrayMessage<T>& message);
 };
 
-}  // namespace behavior_tree_plugin_ros
+}  // namespace behavior_tree_plugin_ros2
 
